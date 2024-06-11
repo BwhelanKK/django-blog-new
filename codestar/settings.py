04b7989a69bba4d6c705b77e0355756cd9bf14d0
Brendan Whelan <whelan_brendan@hotmail.com>
2024-06-11 15:43:42 +0000

    Built Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-bwhelankk-djangoblognew-wf6wiml54kt.ws-eu114.gitpod.io',
                 '.herokuapp.com']
@@ -91,6 +91,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://bwhelankk-djangoblognew-wf6wiml54kt.ws-eu114.gitpod.io/",
+    "https://8000-bwhelankk-djangoblognew-wf6wiml54kt.ws-eu114.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
