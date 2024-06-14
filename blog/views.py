2734b8b9c560a94698e3baa24f9d0fa31347d0ef
Brendan Whelan <whelan_brendan@hotmail.com>
2024-06-14 11:23:58 +0000

    Working with filters and Listview

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all().order_by("-created_on")
     templete_name = "post_list.html"
 
