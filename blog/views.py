5dd394cbc1e24c6b94f1b4b7b3df31e5df890787
Brendan Whelan <whelan_brendan@hotmail.com>
2024-06-15 13:58:41 +0000

    Added templated and CSS

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    # queryset = Post.objects.all().order_by("-created_on")
-    templete_name = "post_list.html"
-
+    template_name = "blog/index.html"
+    paginate_by = 6
