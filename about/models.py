11ba6771a706433a2cf93fad7cf1a1d1c96300ce
Brendan Whelan <whelan_brendan@hotmail.com>
2024-06-20 13:12:11 +0000

    Fix About bugs

--- about/models.py
+++ about/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
     def __str__(self):
         return f"This is the {self.title} section compiled and written by {self.author}."
